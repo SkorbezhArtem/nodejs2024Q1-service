3a42046c5c1347245b28231077f81b43192dfbbb
Artem Skorbezh <artyom.ireday@gmail.com>
2024-03-11 20:15:27 +0300

    init: start HLS Part1 task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
+  console.log(`Server is running on port ${PORT}`);
 }
 bootstrap();
